9722bb03af799f6e82d571f6746612180cb71203
Thibault Saunier <thibault.saunier@collabora.com>
2012-09-24 23:08:46 +0200

    FIXUP Test project

--- tests/check/ges/project.c
+++ tests/check/ges/project.c
@@ -28,21 +28,98 @@ GST_START_TEST (test_project_simple)
   GESProject *project;
   GESTimeline *timeline;
 
-  project =
-      GES_PROJECT (ges_material_new_simple (GES_TYPE_TIMELINE, "timeline1"));
-  fail_unless (GES_IS_PROJECT (project));
-  fail_if (g_strcmp0 (ges_material_get_id (GES_MATERIAL (project)),
-          "timeline1"));
+  ges_init ();
 
-  timeline = GES_TIMELINE (ges_material_extract (GES_MATERIAL (project)));
+  fail_unless (ges_material_new_simple ((GESMaterial **) & project,
+          GES_TYPE_TIMELINE, NULL) == GES_MATERIAL_LOADING_OK);
+  fail_unless (GES_IS_PROJECT (project));
+  assert_equals_string (ges_material_get_id (GES_MATERIAL (project)),
+      "project-0");
+
+  timeline = GES_TIMELINE (ges_material_extract (GES_MATERIAL (project), NULL));
   fail_unless (GES_IS_TIMELINE (timeline));
   id = ges_extractable_get_id (GES_EXTRACTABLE (timeline));
-  fail_if (g_strcmp0 (id, "timeline1"));
+  assert_equals_string (id, "project-0");
 
   g_free (id);
 }
 
 GST_END_TEST;
+
+static void
+material_removed_add_cb (GESProject * project, GESMaterial * material,
+    gboolean * called)
+{
+  *called = TRUE;
+}
+
+GST_START_TEST (test_project_add_materials)
+{
+  GESProject *project;
+  GESMaterial *material;
+  gboolean added_cb_called = FALSE;
+  gboolean removed_cb_called = FALSE;
+
+  ges_init ();
+
+  fail_unless (ges_material_new_simple ((GESMaterial **) & project,
+          GES_TYPE_TIMELINE, NULL) == GES_MATERIAL_LOADING_OK);
+  fail_unless (GES_IS_PROJECT (project));
+  assert_equals_string (ges_material_get_id (GES_MATERIAL (project)),
+      "project-0");
+
+  g_signal_connect (project, "material-added",
+      (GCallback) material_removed_add_cb, &added_cb_called);
+  g_signal_connect (project, "material-removed",
+      (GCallback) material_removed_add_cb, &removed_cb_called);
+
+  fail_unless (ges_material_new_simple (&material,
+          GES_TYPE_TIMELINE_TEST_SOURCE, NULL) == GES_MATERIAL_LOADING_OK);
+  fail_unless (GES_IS_MATERIAL (material));
+
+  fail_unless (ges_project_add_material (project, material));
+  fail_unless (added_cb_called);
+  ASSERT_OBJECT_REFCOUNT (project, "The project", 1);
+  ASSERT_OBJECT_REFCOUNT (material, "The material (1 for project and one for "
+      "main code)", 2);
+
+  fail_unless (ges_project_remove_material (project, material));
+  fail_unless (removed_cb_called);
+  ASSERT_OBJECT_REFCOUNT (material, "The material (only our ref remaining)", 1);
+
+  gst_object_unref (project);
+  gst_object_unref (material);
+}
+
+GST_END_TEST;
+
+/*GST_START_TEST (test_project_load_xptv)*/
+/*{*/
+/*GESProject *project;*/
+
+/*fail_unless (ges_material_new_simple ((GESMaterial **) & project,*/
+/*GES_TYPE_TIMELINE, "") == GES_MATERIAL_LOADING_OK);*/
+/*fail_unless (GES_IS_PROJECT (project));*/
+
+/*g_signal_connect (project, "material-added", (GCallback) material_re,*/
+/*&added_cb_called);*/
+
+/*fail_unless (ges_material_new_simple (&material,*/
+/*GES_TYPE_TIMELINE_TEST_SOURCE, NULL) == GES_MATERIAL_LOADING_OK);*/
+/*fail_unless (GES_IS_MATERIAL (material));*/
+
+/*fail_unless (ges_project_add_material (project, material));*/
+/*fail_unless (added_cb_called);*/
+
+/*ASSERT_OBJECT_REFCOUNT (project, "The project", 1);*/
+/*ASSERT_OBJECT_REFCOUNT (material, "The material (1 for project and one for "*/
+/*"main code)", 2);*/
+
+/*gst_object_unref (project);*/
+/*ASSERT_OBJECT_REFCOUNT (material, "The material (only our ref remaining)", 1);*/
+/*gst_object_unref (material);*/
+/*}*/
+/*GST_END_TEST;*/
 
 static Suite *
 ges_suite (void)
@@ -53,6 +130,7 @@ ges_suite (void)
   suite_add_tcase (s, tc_chain);
 
   tcase_add_test (tc_chain, test_project_simple);
+  tcase_add_test (tc_chain, test_project_add_materials);
 
   return s;
 }
